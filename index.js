abf4e6cf2b5375f65e687a9b0c662ab0e35d15f0
Jessica Hunt <jhunt@twilio.com>
2017-02-01 14:13:21 -0800

    Moving away from Sync for now

--- index.js
+++ index.js
@@ -26,43 +26,45 @@ username for the client requesting a token, and takes a device ID as a query
 parameter.
 
 */
-app.get('/token', function(request, response){
-  var appName = 'TwilioSyncDemo';
-  var identity = randomUsername();
-  console.log(identity);
-  var deviceId = request.query.device;
-
-  // Create a unique ID for the client on their current device
-  var endpointId = `${appName}:${identity}:${deviceId}`;
-  console.log(endpointId);
-
-  // Create a "grant" which enables a client to use Sync as a given user,
-  // on a given device
-  var syncGrant = new SyncGrant({
-    serviceSid: config.service_sid,
-    endpointId: endpointId
-  });
-
-  // Create an access token which we will sign and return to the client,
-  // containing the grant we just created
-  var token = new AccessToken(
-    config.account_sid,
-    config.api_key,
-    config.sync_secret
-  );
-
-  token.addGrant(syncGrant);
-  token.identity = identity;
-  console.log(token);
-  // Serialize the token to a JWT string and include it in a JSON response
-  response.send({
-    identity: identity,
-    token: token.toJwt()
-  });
-});
+// app.get('/token', function(request, response){
+//   var appName = 'TwilioSyncDemo';
+//   var identity = randomUsername();
+//   console.log(identity);
+//   var deviceId = request.query.device;
+//
+//   // Create a unique ID for the client on their current device
+//   var endpointId = `${appName}:${identity}:${deviceId}`;
+//   console.log(endpointId);
+//
+//   // Create a "grant" which enables a client to use Sync as a given user,
+//   // on a given device
+//   var syncGrant = new SyncGrant({
+//     serviceSid: config.service_sid,
+//     endpointId: endpointId
+//   });
+//
+//   // Create an access token which we will sign and return to the client,
+//   // containing the grant we just created
+//   var token = new AccessToken(
+//     config.account_sid,
+//     config.api_key,
+//     config.sync_secret
+//   );
+//
+//   token.addGrant(syncGrant);
+//   token.identity = identity;
+//   console.log(token);
+//   // Serialize the token to a JWT string and include it in a JSON response
+//   response.send({
+//     identity: identity,
+//     token: token.toJwt()
+//   });
+// });
 
 // Twilio Webhook routes
 app.post('/message', message);
+
+
 
 // Ajax route to aggregate response data for the UI
 app.get('/results', results);
